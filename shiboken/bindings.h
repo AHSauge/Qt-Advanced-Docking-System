92cdd65ad9374b2a37547bd7736b3405d4170844
Alf Henrik Sauge <alf.henrik.sauge@gmail.com>
2021-10-14 08:15:22 +0200

    Adding initial attempt to generate PySide2 bindings

---
+++ shiboken/bindings.h
@@ -0,0 +1,25 @@
+
+#ifndef BINDINGS_H
+#define BINDINGS_H
+
+#define QT_ANNOTATE_ACCESS_SPECIFIER(a) __attribute__((annotate(#a)))
+
+#include <DockAreaTabBar.h>
+#include <DockAreaTitleBar.h>
+#include <DockAreaTitleBar_p.h>
+#include <DockAreaWidget.h>
+#include <DockContainerWidget.h>
+#include <DockManager.h>
+#include <DockOverlay.h>
+#include <DockSplitter.h>
+#include <DockWidget.h>
+#include <DockWidgetTab.h>
+#include <DockingStateReader.h>
+#include <DockFocusController.h>
+#include <ElidingLabel.h>
+#include <FloatingDockContainer.h>
+#include <FloatingDragPreview.h>
+#include <IconProvider.h>
+#include <DockComponentsFactory.h>
+
+#endif // BINDINGS_H
